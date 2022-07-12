6eeb27ff06a00d1f818c22ba48770149972cb4fa
Jakob Offersen <jakoboffersen@gmail.com>
2022-07-12 15:27:22 +0200

    wip

---
+++ web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./frontend/components/**/*.{html,js,jsx,ts,tsx}", "./frontend/pages/**/*/.{html,js,jsx,ts,tsx}", "./frontend/index.html", "./frontend/App.tsx"],
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
